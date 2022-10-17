1c085dbf1951046805893c375249d548eb0b3b88
Nigel <m0cvo@yahoo.co.uk>
2022-10-17 11:30:50 +0100

    Starting working on Database schemas.

---
+++ Window4.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MiniOffice
+{
+    /// <summary>
+    /// Interaction logic for Window4.xaml
+    /// </summary>
+    public partial class Window4 : Window
+    {
+        public Window4()
+        {
+            InitializeComponent();
+        }
+
+        private void StaffBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //ToDo
+        }
+
+        private void ClientBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //ToDo
+        }
+
+        private void StockBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //ToDo
+        }
+
+        private void SalesBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //ToDo
+        }
+    }
+}
